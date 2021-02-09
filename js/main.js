3ed4a860c3f573688209688f503010cdebd66853
Andy Duenas <Duenas.g.andy@gmail.com>
2021-02-09 10:59:51 -0800

    Verified that new entries are saved to local storage along with the rest of the data model on page reload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-global-assign */
 /* global data */
 /* exported data */
 
@@ -6,4 +7,36 @@ var $img = document.querySelector('.square-img');
 
 $inputURL.addEventListener('input', function (event) {
   $img.setAttribute('src', event.target.value);
+});
+
+var $title = document.querySelector('#title');
+var $textArea = document.querySelector('#textarea');
+var $form = document.querySelector('.form');
+
+var prevData = localStorage.getItem('jv-local-storage');
+
+if (prevData !== null) {
+  data = JSON.parse(prevData);
+}
+
+$form.addEventListener('submit', function (event) {
+  event.preventDefault();
+  var entryObj = {};
+  entryObj.title = $title.value;
+  entryObj.src = $img.getAttribute('src');
+  entryObj.textArea = $textArea.value;
+  entryObj.dataEntry = data.nextEntryId;
+
+  data.entries.push(entryObj);
+
+  data.nextEntryId++;
+
+  $img.setAttribute('src', 'images/placeholder-image-square.jpg');
+
+  $form.reset();
+});
+
+window.addEventListener('beforeunload', function (event) {
+  var dataJSON = JSON.stringify(data);
+  localStorage.setItem('jv-local-storage', dataJSON);
 });
