18574a8f17c492a269ad60145a55067ad5924267
Andy Duenas <Duenas.g.andy@gmail.com>
2021-02-08 19:57:59 -0800

    added user URL and updated the src

--- js/main.js
+++ js/main.js
@@ -1,2 +1,9 @@
 /* global data */
 /* exported data */
+
+var $inputURL = document.querySelector('#url');
+var $img = document.querySelector('.square-img');
+
+$inputURL.addEventListener('input', function (event) {
+  $img.setAttribute('src', event.target.value);
+});
