d2944b66bd723c6cd290c354adfdf0de39e67f8c
Andy Duenas <Duenas.g.andy@gmail.com>
2021-02-10 12:13:17 -0800

    Defined a function that takes a single journal entry object and returned a DOM tree that matches one of the example entries in the HTML.

--- js/main.js
+++ js/main.js
@@ -27,3 +27,49 @@ $form.addEventListener('submit', function (event) {
 
   $form.reset();
 });
+
+var treeResult = treeMaker(data.entries[0]);
+
+var rowElement = document.querySelector('main');
+
+rowElement.appendChild(treeResult);
+
+function treeMaker(entry) {
+
+  var ulElement = document.createElement('ul');
+  ulElement.setAttribute('class', 'entry-list');
+
+  var liImg = document.createElement('li');
+  liImg.setAttribute('class', 'entry-item');
+  ulElement.appendChild(liImg);
+
+  var imgTag = document.createElement('img');
+  imgTag.setAttribute('class', 'img-entry');
+  imgTag.setAttribute('src', entry.src);
+  liImg.appendChild(imgTag);
+
+  var columnRight = document.createElement('div');
+  columnRight.setAttribute('class', 'entry-column');
+  ulElement.appendChild(columnRight);
+
+  var liTitle = document.createElement('li');
+  liTitle.setAttribute('class', 'entry-item');
+  columnRight.appendChild(liTitle);
+
+  var divTitle = document.createElement('div');
+  divTitle.setAttribute('class', 'title-entry');
+  divTitle.textContent = entry.title;
+  liTitle.appendChild(divTitle);
+
+  var liDescription = document.createElement('li');
+  liDescription.setAttribute('class', 'entry-item');
+  columnRight.appendChild(liDescription);
+
+  var divDescription = document.createElement('div');
+  divDescription.setAttribute('class', 'description-entry');
+  divDescription.textContent = entry.textArea;
+  liDescription.appendChild(divDescription);
+
+  console.log(ulElement);
+  return ulElement;
+}
